ef0f44b24d9f3573a4e55cfc88ea05e84246067c
WalkWithWind <956035125@qq.com>
2018-06-17 17:41:46 +0800

    test
    tet

--- NSStatusBarDemo/AppDelegate.m
+++ NSStatusBarDemo/AppDelegate.m
@@ -9,15 +9,41 @@
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
-
+@property (nonatomic,strong) NSStatusItem *statusItem;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSSquareStatusItemLength];
+    //    NSStatusBarButton *statusBarButton = self.statusItem.button;
+    //    statusBarButton.image = [NSImage imageNamed:@"1.png"];
+    //    [statusBarButton.image setTemplate:YES];
+    //
+    //    statusBarButton.highlighted = NO;
+    //    statusBarButton.toolTip = @"Control-click to quit";
+    //
+    //    [statusBarButton setAction:@selector(handleClick:)];
+    self.statusItem.target = self;
+    self.statusItem.action = @selector(handleClick:);
+    self.statusItem.image = [NSImage imageNamed:@"1"];
+    NSMenu *menu = [[NSMenu alloc]initWithTitle:@"haahh"];
+    [menu addItemWithTitle:@"one" action:@selector(handleItem:) keyEquivalent:@""];
+    [menu addItemWithTitle:@"two" action:@selector(handleItem:) keyEquivalent:@""];
+    [menu addItemWithTitle:@"three" action:@selector(handleItem:) keyEquivalent:@""];
+    self.statusItem.menu = menu;
 }
-
+-(void)handleClick:(NSButton *)button {
+    
+}
+-(void)handleItem:(NSMenuItem *)item {
+    NSLog(@"%@",item.title);
+    if ([item.title isEqualToString:@"one"]) {
+        NSApp.activationPolicy = NSApplicationActivationPolicyAccessory;
+    }else {
+        NSApp.activationPolicy = NSApplicationActivationPolicyRegular;
+    }
+}
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
